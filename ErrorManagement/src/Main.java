9772f7b87a08a6da3a7f329d27f6fd79390c54ae
jdaltamiranodev <jdaltamiranodev@gmail.com>
2022-11-18 10:09:31 -0500

    Introducción a la programación - ErrorManagement

---
+++ ErrorManagement/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int numeros[][] = {
+                {10, 20, 30, 40, 50},
+                {10, 20, 30, 40, 50}
+        };
+        for (int i = 0; i < numeros.length; i++) {
+            for (int j = 0; j < numeros[j].length; j++) {
+                System.out.println(numeros[j]);
+            }
+        }
+    }
+}
