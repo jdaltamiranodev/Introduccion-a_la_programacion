7812ba1003431ac66942d90167906acb9d84d9fa
jdaltamiranodev <jdaltamiranodev@gmail.com>
2022-11-17 22:00:46 -0500

    Introducción a la programación - DoWhile

---
+++ DoWhile/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int contador = 10;
+
+        do {
+            System.out.println(contador);
+            contador--;
+        } while (contador > 1);
+    }
+}
