27758e4e9b2376f0590047152fbbfe75e1f4271d
jdaltamiranodev <jdaltamiranodev@gmail.com>
2022-11-18 18:01:27 -0500

    Introducción a la programación - ProgrammerErrors

---
+++ ProgrammerErrors/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int numeros[] = {10, 20, 30, 40, 50};
+
+        for (int i : numeros){
+            System.out.println(numeros[i]);
+        }
+    }
+}
