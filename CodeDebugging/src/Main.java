b0322da2c1bc0463555a18f2b1ed97c7cb38acad
jdaltamiranodev <jdaltamiranodev@gmail.com>
2022-11-18 10:12:40 -0500

    Introducción a la programación - CodeDebugging

---
+++ CodeDebugging/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int valores[] = new int[5];
+        int posicion = 5;
+
+        System.out.println(valores[posicion]);
+    }
+}
