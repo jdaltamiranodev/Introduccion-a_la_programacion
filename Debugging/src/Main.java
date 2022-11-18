ec8ef2b9a93688d63a5443b7680f63e8c94de95f
jdaltamiranodev <jdaltamiranodev@gmail.com>
2022-11-18 10:11:37 -0500

    Introducción a la programación - Debugging

---
+++ Debugging/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        funcion1();
+    }
+
+    public static void funcion1(){
+        funcion2();
+    }
+
+    public static void funcion2(){
+        funcion3();
+    }
+
+    public static void funcion3(){
+        funcion4();
+    }
+
+    public static void funcion4(){
+        System.out.println("Vaya viaje nos estamos dando");;
+    }
+}
