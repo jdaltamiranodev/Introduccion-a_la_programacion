ea723a73d461994dbf284d2216ac5e0d323036de
jdaltamiranodev <jdaltamiranodev@gmail.com>
2022-11-18 18:00:37 -0500

    Introducción a la programación - ImpactOfErrors

---
+++ ImpactOfErrors/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        /*double temperatura = 15.9;
+        int temperaturaInt = (int)temperatura;
+
+        imprimeTemperatura(temperaturaInt);*/
+
+        double euros = 15.900;
+
+        imprimeEueros(euros);
+    }
+
+    public static void imprimeEueros(double valor){
+        System.out.println(valor);
+    }
+    /*
+    public static void imprimeTemperatura(int valor){
+        System.out.println(valor);
+    }
+     */
+}
