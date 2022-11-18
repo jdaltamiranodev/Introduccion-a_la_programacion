86fea756b2caf1fd833c816789c665c50a497a5e
jdaltamiranodev <jdaltamiranodev@gmail.com>
2022-11-17 22:02:12 -0500

    Introducción a la programación - While

---
+++ While/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int contador = 10;
+
+        while (contador > 0){
+            System.out.println("Contador vale: " + contador);
+            //contador -= 1;
+            contador--;
+        }
+    }
+}
