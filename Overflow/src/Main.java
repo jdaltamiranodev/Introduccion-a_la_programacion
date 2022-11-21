39dfd45bd214d5d851192846cd330e4bf2d1cdea
jdaltamiranodev <jdaltamiranodev@gmail.com>
2022-11-21 18:59:46 -0500

    Introducción a la programación - OverFlow

---
+++ Overflow/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        byte numero = 127;
+        System.out.println(numero);
+
+        numero +=5;
+        System.out.println(numero);
+    }
+}
