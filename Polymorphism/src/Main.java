33d81a8ced6a4823f7ef07a23bc2eb84a2d29e47
jdaltamiranodev <jdaltamiranodev@gmail.com>
2022-11-22 10:00:38 -0500

    Introducción a la programación - Polymorphism

---
+++ Polymorphism/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        Coche coche = new Coche();
+        coche.diHola();
+    }
+}
+
+class Vehiculo {
+    public void diHola(){
+        System.out.println("Hola");
+    }
+}
+
+class Coche extends Vehiculo {
+    public void diHola(){
+        System.out.println("Soy un coche");
+    }
+}
