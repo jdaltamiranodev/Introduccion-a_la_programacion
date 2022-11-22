c49cce72201f002575b032a68a4de9e57c8f9281
jdaltamiranodev <jdaltamiranodev@gmail.com>
2022-11-22 14:42:51 -0500

    Introducción a la programación - Interface

---
+++ Interface/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+interface Vehiculo{
+    void Acelerar(int cuantaVelocidad);
+    void Frenar(int cuantaVelocidad);
+}
+
+class Coche implements Vehiculo {
+    public void Acelerar(int cuantaVelocidad){
+
+    }
+
+    public void Frenar(int cuantaVelocidad){
+
+    }
+}
